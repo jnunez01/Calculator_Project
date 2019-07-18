cc9e2e5c1a3832a51b283cccf002d9c42521dee5
EC2 Default User <ec2-user@ip-172-31-46-157.us-east-2.compute.internal>
2019-07-18 17:37:53 +0000

    mvp

--- calculator.py
+++ calculator.py
@@ -1,1 +1,22 @@
-print("hi")
+
+operation = input('What operation would you like to use?') 
+first_number = input('First number?')
+second_number = input('Second number?')
+
+
+def operation_question(operation):
+    if operation == 'addition':
+        return(int(first_number)+int(second_number))
+    elif operation == 'Addition':
+        return (int(first_number)+int(second_number))
+    elif operation == 'add':
+        return (int(first_number)+int(second_number))
+    elif operation == 'Add':
+        return (int(first_number)+int(second_number))
+    elif operation == '+':
+        return(int(first_number)+int(second_number))
+
+
+
+print(operation_question(operation))
+    
