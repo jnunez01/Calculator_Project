e867f319ceb2f5906c8800502c7ea033d947d99c
EC2 Default User <ec2-user@ip-172-31-46-157.us-east-2.compute.internal>
2019-07-18 18:59:06 +0000

    four function calculator

--- calculator.py
+++ calculator.py
@@ -5,18 +5,41 @@ second_number = input('Second number?')
 
 
 def operation_question(operation):
+    addition_answer = (int(first_number)+int(second_number))
+    subtraction_answer = (int(first_number)-int(second_number))
+    multiplication_answer = (int(first_number)*int(second_number))
+    division_answer = (int(first_number)/int(second_number))
+    operation = operation.lower()
     if operation == 'addition':
-        return(int(first_number)+int(second_number))
+        return(addition_answer)
     elif operation == 'Addition':
-        return (int(first_number)+int(second_number))
+        return (addition_answer)
     elif operation == 'add':
-        return (int(first_number)+int(second_number))
+        return (addition_answer) 
     elif operation == 'Add':
-        return (int(first_number)+int(second_number))
+        return (addition_answer)
     elif operation == '+':
-        return(int(first_number)+int(second_number))
-
-
+        return(addition_answer)
+    elif operation == 'subtraction':
+        return (subtraction_answer)
+    elif operation == 'Subtraction':
+        return(subtraction_answer)
+    elif operation == 'minus':
+        return(subtraction_answer)
+    elif operation == 'Minus':
+        return (subtraction_answer)
+    elif operation == '-':
+        return (subtraction_answer)
+    elif operation == "multiplication" or operation == "Multiplication" or operation == '*' or operation == 'multiply' or operation == 'Multiply' or operation == 'x':
+        return (multiplication_answer)
+    elif operation == 'division' or operation == 'Division' or operation == 'divide' or operation == 'Divide' or operation == '/':
+        return (division_answer)
+    else:
+        return('Try again')
 
 print(operation_question(operation))
+
+
+
+
     
